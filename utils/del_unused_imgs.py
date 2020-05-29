4ee840f46329513b43430c5820f2b3f4e54f1b1c
beaock <beaock@gmail.com>
2020-05-29 14:49:36 +0900

    batch脚本修改

--- utils/del_unused_imgs.py
+++ utils/del_unused_imgs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
-
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #把manage.py所在目录添加到系统目录
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pinry.settings.development'  # 配置系统变量
 import django
 
